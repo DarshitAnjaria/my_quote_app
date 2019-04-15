119f54e1f79e1591b6008b439f3b10668804b323
Darshit Anjaria <darshitanjaria@gmail.com>
2019-04-15 14:10:49 +0530

    My Quote

--- app/src/main/java/com/android/myquotes/AddQuote.kt
+++ app/src/main/java/com/android/myquotes/AddQuote.kt
@@ -2,11 +2,17 @@ package com.android.myquotes
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 
 class AddQuote : AppCompatActivity() {
 
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_quote)
+
+        toolbar = findViewById(R.id.addQuoteToolbar)
+        setSupportActionBar(toolbar)
+
     }
 }
--- app/src/main/java/com/android/myquotes/SplashActivity.kt
+++ app/src/main/java/com/android/myquotes/SplashActivity.kt
@@ -1,12 +1,48 @@
 package com.android.myquotes
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
 
+    val timeOut : Long = 1500
+    private var delayHandler : Handler? = null
+
+    internal val runnable : Runnable = Runnable {
+        if (!isFinishing){
+            val intent = Intent(applicationContext, AddQuote::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        set flags as fullscreen window
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+
+        //initializes the handler
+        delayHandler = Handler()
+
+        //navigate with delay
+        delayHandler!!.postDelayed(runnable, timeOut)
+
+    }
+
+    override fun onDestroy() {
+
+        if (delayHandler != null){
+            delayHandler!!.removeCallbacks(runnable)
+        }
+
+        super.onDestroy()
     }
 }
