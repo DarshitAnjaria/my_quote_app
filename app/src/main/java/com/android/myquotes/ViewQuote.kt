b481890970932c7b9258151fc4c846875c7284a7
Darshit Anjaria <darshitanjaria@gmail.com>
2019-04-16 21:50:36 +0530

    My Quote

---
+++ app/src/main/java/com/android/myquotes/ViewQuote.kt
@@ -0,0 +1,12 @@
+package com.android.myquotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewQuote : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_quote)
+    }
+}
