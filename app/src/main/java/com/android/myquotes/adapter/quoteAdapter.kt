cc9994a2272f9764fdf51cce90246d956b530985
Darshit Anjaria <darshitanjaria@gmail.com>
2019-04-17 12:31:30 +0530

    My Quote

---
+++ app/src/main/java/com/android/myquotes/adapter/quoteAdapter.kt
@@ -0,0 +1,33 @@
+package com.android.myquotes.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.android.myquotes.R
+import com.android.myquotes.model.quotes
+
+class quoteAdapter(val quoteList : ArrayList<quotes>): RecyclerView.Adapter<quoteAdapter.ViewHolder>(){
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvQuote : TextView = itemView.findViewById(R.id.tvShowQuote)
+        val tvAuthor : TextView = itemView.findViewById(R.id.tvShowAuthor)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): quoteAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.itemview, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return quoteList.size
+    }
+
+    override fun onBindViewHolder(holder: quoteAdapter.ViewHolder, position: Int) {
+        holder.tvAuthor.text = quoteList[position].author
+        holder.tvQuote.text = quoteList[position].quote
+    }
+
+}
